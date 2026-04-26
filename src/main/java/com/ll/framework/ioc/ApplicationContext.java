ac19e2b62543c43bfe1bcddac44f4b7b689252af
SHINJAEHEE-DEV <asjh011969@gmail.com>
2026-04-26 18:31:36 +0900

    t3 Green

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -3,14 +3,33 @@ package com.ll.framework.ioc;
 import com.ll.domain.testPost.testPost.repository.TestPostRepository;
 import com.ll.domain.testPost.testPost.service.TestPostService;
 
+import java.util.HashMap;
+
 public class ApplicationContext {
     private TestPostRepository testPostRepository;
+    private final HashMap<String, Object> beansRegistry = new HashMap<>();
+
     public ApplicationContext() {
 
     }
 
     public <T> T genBean(String beanName) {
-        //싱글톤 유지가 되야함
-        return (T) new TestPostService(testPostRepository);
+        if(beansRegistry.containsKey(beanName)) {
+            return (T) beansRegistry.get(beanName);
+        }else{
+            Object bean = createBean(beanName);
+            beansRegistry.put(beanName, bean);
+            return (T) bean;
+        }
+    }
+    private Object createBean(String beanName) {
+        if(beanName.equals("testPostRepository")) {
+            testPostRepository = new TestPostRepository();
+            return testPostRepository;
+        } else if(beanName.equals("testPostService")) {
+            return new TestPostService(testPostRepository);
+        }
+
+        throw new RuntimeException("존재하지 않는 빈 이름입니다.");
     }
 }
